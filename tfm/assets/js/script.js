d1e523b1e323389f8c36e97b0be64ab3663ba45a
Victor Martin Rodriguez <vmrodriguez1995@gmail.com>
2018-05-17 14:25:09 +0000

    Bucle videos youtube

--- tfm/assets/js/script.js
+++ tfm/assets/js/script.js
@@ -1,5 +1,5 @@
 (() => {
-    let obj = obj || {};
+    let obj = {} || obj;
     let youtubeIframeHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/rsWmrGuuWuE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
     
     obj.ajax = (() => {
@@ -29,8 +29,10 @@
                 obj.ajax.getRequest(url, query, function(data) {
                     let results = JSON.parse(data);
                     
-                    results["items"].forEach((i, item) => {
-                        
+                    //console.log(results);
+                    
+                    results["items"].forEach((item, i) => {
+                        console.log("ITEM --> ", item);
                     });
                 });
             });
--- tfm/private/server.js
+++ tfm/private/server.js
@@ -34,7 +34,7 @@ app.get('/search', (req, res) => {
         
         response.on('end', () => {
             res.writeHead(200, {"Content-Type": "text/plain"});
-            res.end(JSON.stringify(data));
+            res.end(data);
         });
     });
 });
